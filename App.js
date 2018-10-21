621b6e46e31bcba3bda5f6de7e18a28181819f81
aymen.masri <aymen.masri@gmail.com>
2018-10-21 20:06:50 +0200

    Update assets

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#44056E',
     alignItems: 'center',
     justifyContent: 'center',
   },
